060c145b06143d1602c163bd3bd6247495783cc7
MarcelMarcelpriPri <marcelfigueredo@gmail.com>
2020-06-01 01:23:53 -0300

    fetch data debug

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,7 +2,7 @@
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 
-unzip packages/regression_model/regression_model/datasets/*.zip
+unzip /home/circleci/project/packages/regression_model/regression_model/datasets/*.zip
 
 echo 'Debugging'
 echo $PWD
