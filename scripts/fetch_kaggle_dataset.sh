a1f35cf27ca504a28b03f64c71fea0853f5fd92e
MarcelMarcelpriPri <marcelfigueredo@gmail.com>
2020-06-07 02:32:01 -0300

    pipeline full section 8.3 with unzip data

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-echo 'ls files 0'
-ls /home/circleci/project/packages/regression_model/regression_model/datasets/
-echo 'unzip'
 unzip packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
-echo 'copying files'
-cp packages/regression_model/regression_model/datasets/*.* /home/circleci/project/packages/regression_model/regression_model/datasets/
 cp *.* /home/circleci/project/packages/regression_model/regression_model/datasets/
-echo 'ls files 1'
-ls packages/regression_model/regression_model/datasets/
-echo 'ls files 2'
+echo 'ls files 0'
 ls /home/circleci/project/packages/regression_model/regression_model/datasets/
