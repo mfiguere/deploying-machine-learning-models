16ea3521515a0bbb572e29020f9c8348bc7056ca
MarcelMarcelpriPri <marcelfigueredo@gmail.com>
2020-06-07 02:26:28 -0300

    test fetch kaggle

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -3,6 +3,8 @@
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 echo 'ls files 0'
 ls /home/circleci/project/packages/regression_model/regression_model/datasets/
+echo 'unzip'
+unzip packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
 echo 'copying files'
 cp packages/regression_model/regression_model/datasets/*.* /home/circleci/project/packages/regression_model/regression_model/datasets/
 echo 'ls files 1'
